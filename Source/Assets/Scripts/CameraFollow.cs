f71e8f0e50bf5b3ae96df4602861219c09bc0016
Hector Pulido <hectorandrespp@gmail.com>
2023-12-01 00:19:03 -0600

    Fixed code

---
+++ Source/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraFollow : MonoBehaviour
+{
+    public static Vector2 LimitsY = new Vector2(-0.594f, -1.636f);
+
+    [SerializeField]
+    public Vector2 limitsY = new Vector2(-0.594f, -1.636f);
+
+    public float offset;
+    public Transform target;
+
+    void Start()
+    {
+        LimitsY = limitsY;
+    }
+
+    void Update()
+    {
+        if (!target)
+            return;
+        transform.position = new Vector3(offset + target.position.x,
+            transform.position.y,
+            transform.position.z);
+    }
+}
---
+++ Source/Assets/Scripts/OrderInLayerEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class OrderInLayerEntity : MonoBehaviour {
+
+    SpriteRenderer sr;
+
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	void Update () {
+        sr.sortingOrder = -(int)(transform.position.y * 100);
+	}
+}
